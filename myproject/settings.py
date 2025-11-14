2d4d8219015f669077a7ee7c0899babc82eb89d2
harinderkmr <harinderjim@gmail.com>
2025-11-14 15:16:42 +0530

    Deployment settings added

--- myproject/settings.py
+++ myproject/settings.py
@@ -116,7 +116,7 @@ STATIC_URL = 'static/'
 #----14-11-2025----
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 STATICFILES_DIRS = [STATIC_DIR]
 MEDIA_URL = "media/"
 MEDIA_ROOT = MEDIA_DIR
